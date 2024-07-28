77930e390a519435a8993cfee248d8630d7d5b63
costya0805 <costya.co4ergin@gmail.com>
2024-07-28 13:50:47 +0500

    ч.2 FP

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -14,38 +14,86 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
+import { allPass } from "ramda";
+import Api from "../tools/api";
 
- const api = new Api();
+const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+// Чистые функции для валидации
+const validLength = (str) => str.length > 2 && str.length < 10;
+const isPositive = (str) => parseFloat(str) > 0;
+const isNumber = (str) => /^[0-9]+\.?[0-9]*$/.test(str);
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+const rules = allPass([validLength, isPositive, isNumber]);
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+// Общая функция валидации
+const validate = (str) => {
+    if (!rules(str)) {
+        throw new Error("ValidationError");
+    }
+    return str;
+};
+// Функция перевода к числу
+const toNumber = (str) => Math.round(parseFloat(str));
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+// Функция вызова API для получения числа в двоичном виде
+const convertToBinary = async (number) => {
+    try {
+        const response = await api.get("https://api.tech/numbers/base", {
+            number,
+            from: 10,
+            to: 2,
+        });
+        return response.result;
+    } catch (e) {
+        throw new Error(e);
+    }
+};
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+// Функция вызова API для получения животного по ID
+const getAnimal = async (id) => {
+    try {
+        const response = await api.get(`https://animals.tech/${id}`, {});
+        return response.result;
+    } catch (e) {
+        throw new Error(e);
+    }
+};
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+// Получение длины строки
+const getLength = (str) => str.length;
+
+// Возведение в квадрат
+const square = (number) => number * number;
+
+// Остаток от деления на 3
+const mod3 = (number) => number % 3;
+
+const processSequence = ({ value, writeLog, handleSuccess, handleError }) => {
+    const writeLogAsync = (num) => {
+        writeLog(num);
+        return num;
+    };
+    const main = (initialStr) => {
+        Promise.resolve(initialStr)
+            .then(writeLogAsync)
+            .then(validate)
+            .then(toNumber)
+            .then(writeLogAsync)
+            .then(convertToBinary)
+            .then(writeLogAsync)
+            .then(getLength)
+            .then(writeLogAsync)
+            .then(square)
+            .then(writeLogAsync)
+            .then(mod3)
+            .then(writeLogAsync)
+            .then(getAnimal)
+            .then((name) => handleSuccess(name))
+            .catch((error) => handleError(error.message));
+    };
+
+    main(value);
+};
 
 export default processSequence;
